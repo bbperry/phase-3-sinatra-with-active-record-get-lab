45af53153257a7f80d4534721da1069b268445bb
Ben <benbperry1@gmail.com>
2021-12-28 18:14:53 -0800

    commit tree e8d45abf15bed675f71a44008bf74c0aa142c1bc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do
+    # get all the games from the database
+    bakery = Bakery.all.order(:title)
+    # return a JSON response with an array of all the game data
+    bakery.to_json
+  end
+
+  get '/bakeries/:id' do
+    # look up the game in the database using its ID
+    bakery = Bakery.find(params[:id])
+    # send a JSON-formatted response of the game data
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    # look up the game in the database using its ID
+    baked_goods = BakedGood.all.order(price: :desc)
+    # send a JSON-formatted response of the game data
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    # look up the game in the database using its ID
+    baked_goods = BakedGood.all.order(price: :desc).first
+    # send a JSON-formatted response of the game data
+    baked_goods.to_json
+  end
+
 
 end
